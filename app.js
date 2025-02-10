70fd119ab55d891aa06ac7a651c4206146a49aec
rithik-choudhary <rithik@transfi.com>
2025-02-10 22:25:31 +0530

    gg

--- app.js
+++ app.js
@@ -1,25 +1,38 @@
+// /Volumes/Macintosh HD/Users/burnt/Documents/workspace/DevOps/Terraform/eatpl/app.js
 const express = require('express');
 const path = require('path');
+const livereload = require('livereload');
+const connectLivereload = require('connect-livereload');
 
 // Import routes
 const indexRouter = require('./routes/index');
+const dashboardRouter = require('./routes/dashboard');
 const apiRouter = require('./routes/api');
 const examHierarchyRoutes = require('./routes/examHierarchy');
 const xlsxTemplateRoutes = require('./routes/xlsxTemplate');
 const directExamRoutes = require('./routes/directExamRoutes');
 const examRouter = require('./routes/exam');
 
+
 const app = express();
 
 // Set up middleware
 app.use(express.json()); // For parsing JSON payloads
 app.use(express.urlencoded({ extended: true })); // For parsing URL-encoded bodies
-app.use(express.static('public'));
+
+// Static file serving - specify the full path
+app.use(express.static(path.join(__dirname, 'public')));
+// If you have CSS in a specific folder, you can also add:
+app.use('/css', express.static(path.join(__dirname, 'public/css')));
+app.use('/images', express.static(path.join(__dirname, 'public/images')));
+app.use('/icons', express.static(path.join(__dirname, 'public/icons')));
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-// Route handlers
+// app.use('/dashboard', dashboardRouter);
 app.use('/api', apiRouter);
+ // This should probably be your main rout.e
 app.use('/', examHierarchyRoutes); 
 app.use('/', xlsxTemplateRoutes);
 app.use('/', examRouter); 
@@ -29,10 +42,8 @@ app.use('/', directExamRoutes);
 // Error handling middleware
 app.use((err, req, res, next) => {
   console.error('Unhandled error:', err);
-  res.status(500).send('Something broke!');
+  res.status(500).send('Something. broke!');
 });
-
-
 
 // const PORT = process.env.PORT || 4;
 // app.listen(PORT, () => {
